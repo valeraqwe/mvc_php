7d31d9f454c53c5373e138c1979f69c29c5568b4
valeraqwe <valera.komar.1999@gmail.com>
2023-09-01 12:27:12 +0300

    Optimize Department model for PHP 8

--- models/Department.php
+++ models/Department.php
@@ -1,16 +1,23 @@
 <?php
+declare(strict_types=1);
+
 require_once __DIR__ . '/../config.php';
 
 class Department {
-    public static function getAll() {
+    public function __construct(
+        public string $name
+    ) {}
+
+    public static function getAll(): array {
         // Retrieve all departments from the database
+        return [];
     }
 
-    public static function create($data) {
+    public static function create(array $data): void {
         // Insert a new department into the database
     }
 
-    public static function delete($id) {
+    public static function delete(int $id): void {
         // Delete a department from the database by its ID
     }
 }
