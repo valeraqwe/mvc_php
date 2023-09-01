5874de2dab7fddd7669e9b090149e444a490d927
valeraqwe <valera.komar.1999@gmail.com>
2023-09-01 12:09:11 +0300

    Add initial view for adding departments

---
+++ views/add_department.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Department</title>
+</head>
+<body>
+<form action="/add-department" method="post">
+    <label for="name">Department Name:</label>
+    <input type="text" id="name" name="name" required><br>
+
+    <input type="submit" value="Add Department">
+</form>
+</body>
+</html>
