fb1b99467eb5d9b65c4da56838ca507d1ad0d1a8
valeraqwe <valera.komar.1999@gmail.com>
2023-09-01 12:12:18 +0300

    Enhance list_users.php to include user and department details

--- views/list_users.php
+++ views/list_users.php
@@ -4,6 +4,22 @@
     <title>List of Users</title>
 </head>
 <body>
-<!-- Display users -->
+<h1>List of Users</h1>
+<table>
+    <thead>
+    <tr>
+        <th>Email</th>
+        <th>Username</th>
+        <th>Address</th>
+        <th>Telephone</th>
+        <th>Comments</th>
+        <th>Department</th>
+        <th>Actions</th>
+    </tr>
+    </thead>
+    <tbody>
+    <!-- Rows will be dynamically populated from the database -->
+    </tbody>
+</table>
 </body>
 </html>
