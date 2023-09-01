9a80684555ed448c91c3909c16bdb4820a30dca0
valeraqwe <valera.komar.1999@gmail.com>
2023-09-01 11:47:19 +0300

    Add database connection settings in config.php

--- config.php
+++ config.php
@@ -1,1 +1,11 @@
 <?php
+$dsn = "mysql:host=localhost;dbname=mydb";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
