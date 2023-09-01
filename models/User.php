eb08e8b1044713e5e91e27ffe98eda7b6130c461
valeraqwe <valera.komar.1999@gmail.com>
2023-09-01 11:52:44 +0300

    Add initial User model for database operations

---
+++ models/User.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace models;
+
+require_once __DIR__ . '/../config.php';
+
+class User {
+    public static function getAll() {
+        // Retrieve all users from the database
+    }
+
+    public static function create($data) {
+        // Insert a new user into the database
+    }
+}
