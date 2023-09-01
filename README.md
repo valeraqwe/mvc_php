a35b784ab7ca6b2dd6bea86e75a7d5194e25a71e
valeraqwe <valera.komar.1999@gmail.com>
2023-09-01 12:25:19 +0300

    Optimize User model for PHP 8

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# PHP MVC Project
+
+This is a simple PHP MVC project for managing users and departments.
+
+## Features
+
+- Add new users with details like email, username, address, etc.
+- List all users and their associated departments.
+- Add new departments and delete existing ones.
+
+## Setup
+
+1. Clone the repository.
+2. Import the database schema.
+3. Configure the `config.php` file for your database settings.
+4. Run the project using a PHP server.
+
--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,18 +1,24 @@
 <?php
-require_once __DIR__ . '/../models/User.php';
+declare(strict_types=1);
 
-function listUsers() {
-    $users = User::getAll();
-    require __DIR__ . '/../views/list_users.php';
-}
+require_once __DIR__ . '/../config.php';
 
-function addUser() {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        User::create($_POST);
+class User {
+    public function __construct(
+        public string $email,
+        public string $username,
+        public ?string $address = null,
+        public ?string $telephone = null,
+        public ?string $comments = null,
+        public ?string $department = null
+    ) {}
+
+    public static function getAll(): array {
+        // Retrieve all users from the database
+        return [];
     }
-    require __DIR__ . '/../views/add_user.php';
-}
 
-function viewUser() {
-    // Implement view logic
+    public static function create(array $data): void {
+        // Insert a new user into the database
+    }
 }
