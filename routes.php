16b8854d060af2eea8bed28aa99a7bfdb1f3d016
valeraqwe <valera.komar.1999@gmail.com>
2023-09-01 11:48:55 +0300

    Add initial routing logic in routes.php

--- routes.php
+++ routes.php
@@ -1,1 +1,18 @@
 <?php
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/users':
+        require __DIR__ . '/controllers/UserController.php';
+        listUsers();
+        break;
+    case '/add-user':
+        require __DIR__ . '/controllers/UserController.php';
+        addUser();
+        break;
+    case preg_match('/\/user\/\d+/', $request) ? $request : !$request:
+        require __DIR__ . '/controllers/UserController.php';
+        viewUser();
+        break;
+    // Add more routes
+}
