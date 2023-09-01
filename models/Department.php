526a663add30c9cac3f16c0970a079ff7b5eb434
valeraqwe <valera.komar.1999@gmail.com>
2023-09-01 12:05:27 +0300

    Add initial Department model for database operations

---
+++ models/Department.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '/../config.php';
+
+class Department {
+    public static function getAll() {
+        // Retrieve all departments from the database
+    }
+
+    public static function create($data) {
+        // Insert a new department into the database
+    }
+
+    public static function delete($id) {
+        // Delete a department from the database by its ID
+    }
+}
